f73f039279cdf1ab70d44560c4bf4e50744c224e
Hemal Tharaka Eranaga <156353444+HemalTharakaeEranga@users.noreply.github.com>
2024-06-11 06:12:54 +0000

    The Locunda level 5 vulnerable codefix in code.py

--- Season-1/Level-5/code.py
+++ Season-1/Level-5/code.py
@@ -3,7 +3,6 @@
 # This is the last level of our first season, good luck!
 
 import binascii
-import random
 import secrets
 import hashlib
 import os
@@ -17,17 +16,19 @@ class Random_generator:
     'abcdefghijklmnopqrstuvwxyz'
     'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     )):
-        return ''.join(random.choice(alphabet) for _ in range(length))
+        return ''.join(secrets.choice(alphabet) for _ in range(length))
 
     # generates salt
     def generate_salt(self, rounds=12):
-        salt = ''.join(str(random.randint(0, 9)) for _ in range(21)) + '.'
-        return f'$2b${rounds}${salt}'.encode()
+        salt = bcrypt.gensalt(rounds=rounds)
+        return salt
 
 class SHA256_hasher:
 
-    # produces the password hash by combining password + salt because hashing
-    def password_hash(self, password, salt):
+    # produces the password hash using bcrypt
+    def password_hash(self, password, salt=None):
+        if salt is None:
+            salt = bcrypt.gensalt()
         password = binascii.hexlify(hashlib.sha256(password.encode()).digest())
         password_hash = bcrypt.hashpw(password, salt)
         return password_hash.decode('ascii')
@@ -38,6 +39,7 @@ class SHA256_hasher:
         password_hash = password_hash.encode('ascii')
         return bcrypt.checkpw(password, password_hash)
 
+# Deprecated MD5_hasher for security reasons
 class MD5_hasher:
 
     # same as above but using a different algorithm to hash which is MD5
@@ -46,13 +48,17 @@ class MD5_hasher:
 
     def password_verification(self, password, password_hash):
         password = self.password_hash(password)
-        return secrets.compare_digest(password.encode(), password_hash.encode())
+        return secrets.compare_digest(password, password_hash)
 
 # a collection of sensitive secrets necessary for the software to operate
 PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
 PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
-SECRET_KEY = 'TjWnZr4u7x!A%D*G-KaPdSgVkXp2s5v8'
-PASSWORD_HASHER = 'MD5_hasher'
+SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_urlsafe(32))  # Default to a secure random key if not set
+PASSWORD_HASHER = 'SHA256_hasher'
+
+# Ensure SECRET_KEY is securely managed and not hardcoded.
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable is not set!")
 
 
 # Contribute new levels to the game in 3 simple steps!
